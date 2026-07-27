ac84d2c26afb80601b611c56c9119f13ed39ec02
Mayank Upadhyay <mupadhyay1302@gmail.com>
2026-07-27 19:21:05 -0400

    Fix names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Harsh Upadhyay",
-  description: "Computer science student at McMaster University. Full stack developer & software engineer.",
+  title: "Mayank Upadhyay",
+  description: "Software Engineering student at University Of Waterloo. Full stack developer & software engineer.",
   openGraph: {
-    title: "Harsh Upadhyay",
-    description: "Computer science student at McMaster University.",
+    title: "Mayank Upadhyay",
+    description: "Software Engineering student at University Of Waterloo.",
     type: "website",
   },
 };
